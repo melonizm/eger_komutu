94c4426b392954b3dc8fd6a6d4025029e5e926b4
melo <120278255+melonizm@users.noreply.github.com>
2022-12-10 23:31:37 +0300

    if komutu
    en basit şekilde if komutu

---
+++ eger/if_komutu/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+       int sayi = 24;
+       if (sayi < 20) {
+           System.out.println("sayı yirmiden küçüktür");
+       }else if (sayi==20){
+           System.out.println("sayı yirmiye eşittir");
+       } else {
+           System.out.println("sayı yirmiden büyüktür");
+       }
+
+
+    }
+}
